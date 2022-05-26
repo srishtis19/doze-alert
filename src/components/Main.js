918b88f7c52fc7c83afff2411224981c48489486
Srishti <srishtis0507@gmail.com>
2022-05-26 11:48:20 +0530

    handle monitoring using button

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,42 +15,46 @@ export default function Main(){
 
         const webcamRef = React.useRef(null);
         const [state, setState] = React.useState("Focussed");
+        const [isMonitoring, setIsMonitoring] = React.useState(false)
 
         const submitImage = (blob) =>{
-            let data = new FormData;
-            let URL = "http://localhost:8080/sendImageBlob";
-            //console.log(blob)
-            data.append('file',blob,'face_image');
-            //console.log(...data) 
+            // let data = new FormData;
+            // let URL = "http://localhost:8080/sendImageBlob";
+            // //console.log(blob)
+            // data.append('file',blob,'face_image');
+            // //console.log(...data) 
             
-            axios.post(URL, data)
-            .then(response => {
-                //console.log('response', response)
-                const faceAttributes = response.data
-                console.log(faceAttributes)
-                if(!faceAttributes.EyesOpen.Value){
-                    setState("Drowsiness Detected!")
-                }
-                else if(faceAttributes.MouthOpen.Value){
-                    setState("Yawn Detected!")
-                }
-                else {
-                    setState("Focussed")
-                }
-              }).catch(error => {
-                console.log('error', error)
-              })
+            // axios.post(URL, data)
+            // .then(response => {
+            //     //console.log('response', response)
+            //     const faceAttributes = response.data
+            //     console.log(faceAttributes)
+            //     if(!faceAttributes.EyesOpen.Value){
+            //         setState("Drowsiness Detected!")
+            //     }
+            //     else if(faceAttributes.MouthOpen.Value){
+            //         setState("Yawn Detected!")
+            //     }
+            //     else {
+            //         setState("Focussed")
+            //     }
+            //   }).catch(error => {
+            //     console.log('error', error)
+            //   })
+            console.log("submitted!")
         }
 
         React.useEffect(() => {
-            const interval = setInterval(() => {
-              const imageSrc = webcamRef.current.getScreenshot();
-              const blob = makeblob(imageSrc)
-              //console.log(blob)
-              submitImage(blob);
-            }, 5000);
-            return () => clearInterval(interval);
-          }, []);
+            if(isMonitoring){
+                const interval = setInterval(() => {
+                const imageSrc = webcamRef.current.getScreenshot();
+                const blob = makeblob(imageSrc)
+                //console.log(blob)
+                submitImage(blob);
+                }, 5000);
+                return () => clearInterval(interval);
+            }
+        }, [isMonitoring]);
 
         const {
             seconds,
@@ -61,6 +65,17 @@ export default function Main(){
             pause,
             reset,
           } = useStopwatch({ autoStart:false});
+        
+        const handleMonitoring = () =>{
+            if(isRunning){
+                pause();
+            }
+            else{
+                start();
+            }
+            setIsMonitoring((prevState)=>!prevState)
+        }
+
 
     return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
@@ -100,14 +115,6 @@ export default function Main(){
                     screenshotFormat="image/jpeg"
                 />
             </Box>
-            {/* <Webcam
-                width={800}
-                height={600}
-                className="webcam"
-                audio={false}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-            /> */}
             <Button 
                 variant="contained" 
                 size="large"
@@ -121,7 +128,7 @@ export default function Main(){
                     textTransform:'none',
                 }} 
                 startIcon={<HourglassTopIcon />}
-                onClick={isRunning? pause:start}
+                onClick = {handleMonitoring}
                 //endIcon={<ArrowRightOutlinedIcon />}
             >
                 <Typography variant="h6"> {isRunning? `Take a break!`: `Start Focussing >>`} </Typography>  
