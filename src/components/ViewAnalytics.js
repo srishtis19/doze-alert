64faab029af0c308b1e76093ca15a9c344d1d239
Srishti <srishtis0507@gmail.com>
2022-05-29 23:21:14 +0530

    minor changes

--- src/components/ViewAnalytics.js
+++ src/components/ViewAnalytics.js
@@ -170,7 +170,6 @@ export default function ViewAnalytics(){
                 justifyContent='space-between'
                 sx={{
                     alignItems:'center',
-                    marginTop:'100px',
                 }}
             >
 
@@ -244,7 +243,7 @@ export default function ViewAnalytics(){
                 height={350} 
                 width={600}
              />
-             
+
         </Box>
     )
 }
